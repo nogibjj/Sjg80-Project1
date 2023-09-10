c727b23f53fbfe8d796d468cb7251209e3d4bfa4
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-09-10 14:15:55 -0400

    Update PandasScript.py

--- PandasScript.py
+++ PandasScript.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import io
+import pdfkit
 
 # Step 2: Read a dataset CSV
 import seaborn as sns
@@ -18,3 +20,18 @@ plt.show()
 
 # Save summary statistics to a CSV file
 summary_statistics.to_csv("summary_statistics.csv", index=False)
+
+# Create a PDF file
+pdf = pdfkit.PDF()
+
+# Write the summary statistics to the PDF file
+with io.open('summary_statistics.pdf', 'w', encoding='utf-8') as f:
+    pdf.write(f, orientation='landscape')
+
+# Create a Markdown file
+markdown = "## Summary Statistics"
+markdown += iris.describe().to_markdown()
+
+# Write the summary statistics to the Markdown file
+with open('summary_statistics.md', 'w') as f:
+    f.write(markdown)
