0da78936185d68768187cb651aae286734842170
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-09-10 11:47:09 -0400

    Update PandasScript.py

--- PandasScript.py
+++ PandasScript.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
 
+# Load the Iris dataset
+iris = load_iris()
+df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+
+# Generate summary statistics
+summary_stats = df.describe()
+
+# Create a data visualization
+sns.pairplot(df, hue=iris.target_names[iris.target])
+plt.show()
+
+print(summary_stats)
