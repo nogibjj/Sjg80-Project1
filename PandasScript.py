004ec86c17d72c0049e2c8da0d88afe2659a53f6
Shaila Janeth Guereca Guzman <shailaguereca@MBP-de-Shaila.lan>
2023-09-10 12:36:41 -0400

    Print summary statistics

--- PandasScript.py
+++ PandasScript.py
@@ -1,19 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Step 2: Read a dataset from a built-in library (e.g., Seaborn)
+# Step 2: Read a dataset from a built-in library (Seaborn)
 import seaborn as sns
-iris = sns.load_dataset('iris')
+
+iris = sns.load_dataset("iris")
 
 # Step 3: Generate summary statistics
 summary_statistics = iris.describe()
+print(summary_statistics)
 
 # Step 4: Create a data visualization (e.g., histogram)
-sns.histplot(iris['sepal_length'], kde=True)
-plt.title('Distribution of Sepal Length')
-plt.xlabel('Sepal Length')
-plt.ylabel('Frequency')
+sns.histplot(iris["sepal_length"], kde=True)
+plt.title("Distribution of Sepal Length")
+plt.xlabel("Sepal Length")
+plt.ylabel("Frequency")
 plt.show()
 
-# Save summary statistics to a CSV file (optional)
-summary_statistics.to_csv('summary_statistics.csv', index=False)
+# Save summary statistics to a CSV file
+summary_statistics.to_csv("summary_statistics.csv", index=False)
