53b725bdd0aed919de592c9f6c05d8b5d6f98e99
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-09-22 12:26:56 -0400

    Update test_lib.py

--- test_lib.py
+++ test_lib.py
@@ -7,47 +7,12 @@ from lib import boxplot_of_cols
 def test_calc_desc_stat():
     
     df = pd.DataFrame({'data': [1, 2, 3, 4, 5]})
-    if dataset_col is None: 
-        return "Please input a dataframe column"
-    out=dataset_col.describe()
-    return out
-
     output = calc_desc_stat(df['data'])
     expected_output = df['data'].describe()
-
     assert output == expected_output
 
-    with pytest.raises(TypeError):
-        calc_desc_stat(None)
+def test_boxplot_of_cols():
 
-def test_boxplot_of_cols(
-        df_wanted=None, col1=None, col2=None, col3=None, file_name=None
-):
-    
-    if isinstance(df_wanted, pd.DataFrame):
-        print("good to go")
-    else:
-        return "This is the wrong input, you have " + str(type(df_wanted))
-    list_of_columns = []
-    if col1:
-        list_of_columns.append(col1)
-    if col2:
-        list_of_columns.append(col2)
-    if col3:
-        list_of_columns.append(col3)
-    if len(list_of_columns) == 0:
-        print("There are no columns to plot")
-        return
-    
-    df_to_plot = df_wanted[list_of_columns]  
-    df_to_plot.boxplot()
-    
-    if file_name:
-        plt.savefig(file_name)  
-    # Save the figure before showing it
-    plt.savefig("boxplot")
-    plt.show()
-    
     # Create a DataFrame with some data
     df = pd.DataFrame({'data1': [1, 2, 3, 4, 5], 'data2': [6, 7, 8, 9, 10]})
 
